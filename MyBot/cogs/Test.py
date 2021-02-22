538bf984a3aa8d3a4959dc29f26bbe8c50384592
Sweet1e42 <64321279+Sweet1e42@users.noreply.github.com>
2021-02-23 00:02:02 +0300

    Update Test.py

--- MyBot/cogs/Test.py
+++ MyBot/cogs/Test.py
@@ -5,10 +5,39 @@ class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-# reply
-    @commands.command(name = "reply")
-    async def command_reply(self, ctx, *, text):
-        await ctx.send(text)
+# Kick
+    @commands.command(pass_context = True, name = "Kick")
+    async def mod_kick(self, ctx, user: discord.Member = None, *, reason = "Отсуствует"):
+        if not user:
+            return
+        if user == self.bot.user:
+            return
+        if user == ctx.author:
+            return
+        if user.top_role >= ctx.author.top_role:
+            return
+        if not ctx.author.guild_permissions.kick_members:
+            return
+
+        try:
+            await user.kick(reason = f"{ctx.author}, reason: {reason}")
+
+        except discord.Forbidden:
+            return
+
+        except discord.HTTPException:
+            return
+
+        now = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M %d.%m')
+
+        form = (
+            f"Кикнут: {user.mention} \n"
+            f"Кикнул: {ctx.author.mention} \n"
+            f"Причина: {reason} \n"
+            f"Время: {now}"
+        )
+
+        await ctx.send(embed = discord.Embed(title = "Модерация | Кик", description = form, color = 0xff033))
 
 def setup(bot):
     bot.add_cog(Test(bot))
