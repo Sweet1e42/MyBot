d8c826d63c271512b152ceb714cdcf2ed6d1c88c
Sweet1e42 <64321279+Sweet1e42@users.noreply.github.com>
2021-02-23 08:59:01 +0300

    Обновлённый бот, читайте README.md

---
+++ MyBot/cogs/Admin.py
@@ -0,0 +1,50 @@
+# Основные библиотеки
+import discord
+from discord.ext import commands
+
+# Доп. библиотеки
+import time
+import datetime
+
+# Класс Admin
+class Admin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+# Kick
+    @commands.command(pass_context = True, name = "Kick")
+    async def mod_kick(self, ctx, user: discord.Member = None, *, reason = "Отсуствует"):
+        if not user:
+            return
+        if user == self.bot.user:
+            return
+        if user == ctx.author:
+            return
+        if user.top_role >= ctx.author.top_role:
+            return
+        if not ctx.author.guild_permissions.kick_members:
+            return
+
+        try:
+            await user.kick(reason = f"{ctx.author}, reason: {reason}")
+
+        except discord.Forbidden:
+            return
+
+        except discord.HTTPException:
+            return
+
+        now = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M %d.%m')
+
+        form = (
+            f"Кикнут: {user.mention} \n"
+            f"Кикнул: {ctx.author.mention} \n"
+            f"Причина: {reason} \n"
+            f"Время: {now}"
+        )
+
+        await ctx.send(embed = discord.Embed(title = "Модерация | Кик", description = form, color = 0xff033))
+
+# Инцилизация бота
+def setup(bot):
+    bot.add_cog(Admin(bot))
--- MyBot/main.py
+++ MyBot/main.py
@@ -1,24 +1,34 @@
+# Основные библиотеки
 import discord
 from discord.ext import commands
 
+# Файл конфигурации
+import setting.config
+from setting.config import setting
+
+# Коги
 cogs = [
-    "Test"
+    "Admin"
 ]
 
+# Класс Main
 class Main(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+# Переменная Client к которой мы будем обращаться будущем
 client = Main(
-    command_prefix = "/",
+    command_prefix = setting["PREFIX"],
     intents = discord.Intents.all(),
     help_command = None
 )
 
+# Вывод в консоль сообщения о включении
 @client.event
 async def on_ready():
     print(f"{client.user.name} Is a ready")
 
+# Инициализация Main файла + загрузка когов
 if __name__ == "__main__":
     for extension in cogs:
         cog = f"cogs.{extension}"
@@ -27,4 +37,5 @@ if __name__ == "__main__":
         except Exception as e:
             print(e)
 
-client.run("ЗДЕСЬ_ТОКЕН")
+# Старт бота
+client.run(setting["TOKEN"])
