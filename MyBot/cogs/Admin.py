0f3d2ace197d6550d17d07857f80e9b4b12b7df5
Sweet1e42 <64321279+Sweet1e42@users.noreply.github.com>
2021-02-25 07:19:25 +0300

    Add files via upload

--- MyBot/cogs/Admin.py
+++ MyBot/cogs/Admin.py
@@ -45,6 +45,40 @@ class Admin(commands.Cog):
 
         await ctx.send(embed = discord.Embed(title = "Модерация | Кик", description = form, color = 0xff033))
 
+# Ban
+    @commands.command(name = "ban")
+    async def mod_ban(self, ctx, user: discord.Member = None, *, reason = "Отсуствует"):
+        if not user:
+            return
+        if user == self.bot.user:
+            return
+        if user == ctx.author:
+            return
+        if user.top_role >= ctx.author.top_role:
+            return
+        if not ctx.author.guild_permissions.kick_members:
+            return
+
+        try:
+            await user.ban(reason = f"{ctx.author}, reason: {reason}")
+
+        except discord.Forbidden:
+            return
+
+        except discord.HTTPException:
+            return       
+
+        now = datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M %d.%m')
+
+        form = (
+            f"Забанен: {user.mention} \n"
+            f"Забанил: {ctx.author.mention} \n"
+            f"Причина: {reason} \n"
+            f"Время: {now} \n"
+        )
+
+        await ctx.send(embed = discord.Embed(title = "Модерация | Бан", description = form, color = 0xff033))
+
 # Инцилизация бота
 def setup(bot):
     bot.add_cog(Admin(bot))
